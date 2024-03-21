c4dcf89092dfa9122fd9c22eef5957c4169b1ce6
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-21 20:45:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,9 @@ import { useRef } from "react"
 
 const App = () => {
 
-  const [data,setData] = useState([]);
-  const inputRef = useRef(null);
-
   return (
-    <div className="useref">
-      <div className="ur-header">Use-Ref</div>
-      <input ref={inputRef} type="text"/>
-      <button onClick={()=>{setData([...data,inputRef.current.value])}}>Submit</button>
-      {data.map((item,index)=>{return <h2 key={index}>{item}</h2>})}
+    <div>
+      <Ur/>
     </div>
   )
 }
