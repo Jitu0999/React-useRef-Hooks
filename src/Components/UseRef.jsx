22ed8f417a36abea65a1a477d86baaada5b167ba
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-22 21:19:18 +0530

    Create UseRef.jsx

---
+++ src/Components/UseRef.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useRef } from "react"
+import './CSS/UseRef.css'
+import UseRefItems from './UseRefItems';
+
+const App = () => {
+
+  const [data,setData] = useState([]);
+  const inputRef = useRef(null);
+
+  return (
+    <div className="us-ref">
+      <div className="us-ref-header">Use-Ref</div>
+      <input ref={inputRef} type="text"/>
+      <button onClick={()=>{setData([...data,inputRef.current.value])}}>Submit</button>
+      {data.map((item,index)=>{return <h2 key={index}>{item}</h2>})}
+    </div>
+  )
+}
+
+export default App
